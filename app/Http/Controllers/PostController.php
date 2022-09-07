adc634a1ef239dbdc32a51daad94ca0f7b96e02d
egashiramikito <br120017@sun.ac.jp>
2022-09-07 17:55:07 +0900

    記事の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,6 @@ class PostController extends Controller
     public function index(Post $post)
    {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
-
    } 
    
    public function show(Post $post)
@@ -18,8 +17,21 @@ class PostController extends Controller
        return view('posts/show')->with(['post'=>$post]);
    }
     
+    public function create()
+    {
+        return view('posts/create');
+    }
     
-    
+    public function store(Post $post, Request $request)
+    {
+   
+{
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+}
+        dd($request->all());
+    }
 }
 
 
