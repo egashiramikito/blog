949b8ab0186d146119c42dbac12580a1065d8d54
egashiramikito <br120017@sun.ac.jp>
2022-09-08 16:08:12 +0900

    記事の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,14 +24,24 @@ class PostController extends Controller
     
     public function store(Post $post, PostRequest $request)
     {
-   
-{
+    {
     $input = $request['post'];
     $post->fill($input)->save();
     return redirect('/posts/' . $post->id);
-}
+    }
         dd($request->all());
     }
+    
+    public function edit(Post $post)
+{
+    return view('posts/edit')->with(['post' => $post]);
+}
+   public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+    return redirect('/posts/' . $post->id);
+} 
 }
 
 
