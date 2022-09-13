62a863cbf60db4233033767813b3cb240c45cad3
egashiramikito <br120017@sun.ac.jp>
2022-09-13 17:48:09 +0900

    記事の削除

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,6 +9,15 @@
     <body>
         <h1>Blog Name</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_delete" method="post">
+          @csrf
+          @method('DELETE')
+        
+        <button type="submit" onClick="deletePost();return false;">delete</button> 
+       
+
+
+    </form>
         <div class='post'>
             <h2 class='title'>{{ $post->title }}</h2>
             <p class='body'>{{ $post->body }}</p>
@@ -16,5 +25,13 @@
        
         </div>
         <div class='back'>[<a href='/'>back</a>]</div>
+        <script>
+            function deletePost(e) {
+                'use strict';
+                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById('from_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
